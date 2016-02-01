0a8785a7fcb154e7df03f5e24fc9d62ed644cdad
jebjohnson <jebjohnson@jebs-air.mobile.asu.edu>
2016-02-01 13:17:32 -0700

    The side bar buttons have been added. I also added two additional classes TimeSheetPanel.java and DefectLogPanel.java. This was just to test the functionality of the two buttons added. They can be edited or deleted.

--- src/net/sf/memoranda/ui/WorkPanel.java
+++ src/net/sf/memoranda/ui/WorkPanel.java
@@ -21,7 +21,7 @@ import net.sf.memoranda.util.Local;
 
 /**
  * 
- * ***:Copyright (c) 2003 Memoranda Team. http://memoranda.sf.net
+ * Copyright (c) 2003 Memoranda Team. http://memoranda.sf.net
  */
 
 /*$Id: WorkPanel.java,v 1.9 2004/04/05 10:05:44 alexeya Exp $*/
