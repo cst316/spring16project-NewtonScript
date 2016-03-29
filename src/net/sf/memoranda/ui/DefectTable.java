8ae19c00c833c566a8c0cf73f9229b65285ce99d
jerss2k <dcarrol3@asu.edu>
2016-03-28 18:09:56 -0700

    Merge check
    - Paired programming Jeb and Doug.

--- src/net/sf/memoranda/ui/DefectTable.java
+++ src/net/sf/memoranda/ui/DefectTable.java
@@ -29,7 +29,12 @@ public class DefectTable extends javax.swing.JPanel {
     }
 
     
-    public static DefectFunctionality getFunctionality(){
+    public static javax.swing.JTabbedPane getjTabbedPane() {
+		return jTabbedPane1;
+	}
+
+
+	public static DefectFunctionality getFunctionality(){
     	return func;
     } 
     /**
@@ -377,7 +382,7 @@ public class DefectTable extends javax.swing.JPanel {
    private javax.swing.JScrollPane jScrollPane2;
    private javax.swing.JScrollPane jScrollPane3;
    private javax.swing.JScrollPane jScrollPane4;
-   private javax.swing.JTabbedPane jTabbedPane1;
+   private static javax.swing.JTabbedPane jTabbedPane1;
    private static javax.swing.JTable openDefectTable;
    private javax.swing.JTable jTable2;
    private javax.swing.JTable jTable3;
